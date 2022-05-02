79e898084aff073b7abdec8acde2ab0d37f6b736
Andrey Gruzdov <gruzdov.2016.stud@mail.ru>
2022-05-02 23:30:53 +0300

    Revert "Создал дополнительные методы в JsonMapper.java"
    This reverts commit 3d64649b249288edc398b953450c328c4c50eae4.

--- src/main/java/ru/mpei/mapper/JsonMapper.java
+++ src/main/java/ru/mpei/mapper/JsonMapper.java
@@ -7,8 +7,6 @@ import ru.mpei.SingelLineDiagram;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JsonMapper {
 
@@ -18,31 +16,5 @@ public class JsonMapper {
     @SneakyThrows /**Try cetch */
     public SingelLineDiagram mapJasonToSld (String filePath) {
         return objectMapper.readValue( new File(filePath), SingelLineDiagram.class);
-    }
-    @SneakyThrows
-    public SingleLineDiagram mapJsonToSld(String filePath) {
-        return objectMapper.readValue(
-                new File(filePath), SingleLineDiagram.class
-        );
-    }
-
-    @SneakyThrows
-    public Map<String, String> mapDeviceJsonToSld(String filePath) {
-        Map<String, String> device = new HashMap<>();
-        Device[] deviceType = objectMapper.readValue(new File(filePath), Device[].class);
-        for (Device line : deviceType) {
-            device.put(line.getId(), line.getName().getEn());
-        }
-        return device;
-    }
-
-    @SneakyThrows
-    public Map<String, String> mapVoltageLevelJsonToSld(String filePath) {
-        Map<String, String> voltage = new HashMap<>();
-        Voltage[] voltageType = objectMapper.readValue(new File(filePath), Voltage[].class);
-        for (Voltage line : voltageType) {
-            voltage.put(line.getId(), line.getValue().getEn());
-        }
-        return voltage;
     }
 }
